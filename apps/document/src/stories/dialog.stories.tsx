04524288c5a2af5b3bf08db4c6824002fa69e917
Alison Huang <alisonhuang@Migo-Alison-MacPro.local>
2022-05-20 11:22:16 +0800

    style(react-): update react dialog project.json file | Update react button package.json file

--- apps/document/src/stories/dialog.stories.tsx
+++ apps/document/src/stories/dialog.stories.tsx
@@ -11,4 +11,5 @@ const Template: Story<ReactDialogProps> = (args) => <ReactDialog {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   dialog: true,
+  children: <div>Check</div>
 };
