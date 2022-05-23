7d7c16af94dd3633d59a25764ebad962d50f9db6
Yuri Tsai <yuri.t@migo.io>
2022-05-23 15:16:32 +0800

    fix(react-toast): correct import path

--- apps/document/src/stories/toast.stories.tsx
+++ apps/document/src/stories/toast.stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react';
-import { ReactToast, ReactToastProps } from '@paintbox/react/toast';
+import { ReactToast, ReactToastProps } from '@paintbox/react-toast';
 
 export default {
   component: ReactToast,
