0187e7480e4feb7a611e8b9cf89294d225ebb45d
Yuri Tsai <showwell0120@gmail.com>
2022-08-24 12:04:38 +0800

    feat(react-select-option): add package

--- packages/react/form/src/index.ts
+++ packages/react/form/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/react-form';
 export * from '@paintbox/react-checkbox';
 export * from '@paintbox/react-time-picker';
 export * from '@paintbox/react-search-bar';
+export * from '@paintbox/react-select-option';
