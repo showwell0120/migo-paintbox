8ec5afbfa7a6a4ddd5667bff4e2d06595ce0892a
Yuri Tsai <showwell0120@gmail.com>
2022-08-25 16:37:04 +0800

    feat(react-pagination): add package

---
+++ packages/react/pagination/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'react-pagination',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/packages/react/pagination',
+};
