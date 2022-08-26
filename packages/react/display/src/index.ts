3d1e81e8e15f6fc669c3a4f075f62fba6c48b0a4
Yuri Tsai <showwell0120@gmail.com>
2022-08-26 17:26:25 +0800

    fix(react-*): remove unused files | update export/import

--- packages/react/display/src/index.ts
+++ packages/react/display/src/index.ts
@@ -1,5 +1,3 @@
-export * from './lib/react-display';
-
 export * from '@paintbox/react-chip';
 export * from '@paintbox/react-tag';
 export * from '@paintbox/react-tree-view';
--- packages/react/ui/src/index.ts
+++ packages/react/ui/src/index.ts
@@ -1,4 +1,5 @@
-export * from './lib/react-ui';
-
 export * from '@paintbox/react-foundation';
 export * from '@paintbox/react-form';
+export * from '@paintbox/react-feedback';
+export * from '@paintbox/react-table';
+export * from '@paintbox/react-display';
