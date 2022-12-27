f81604aa7c30bc809f09eaa33effa07e75502aff
Yuri Tsai <showwell0120@gmail.com>
2022-12-27 18:23:04 +0800

    feat(react-foundation): add color related css selectors

--- packages/react/foundation/src/common.ts
+++ packages/react/foundation/src/common.ts
@@ -3,3 +3,40 @@ export interface SVGProps {
   fill?: string;
   stroke?: string;
 }
+
+export type Variant = 'contained' | 'outlined';
+
+// @see https://stackoverflow.com/questions/59857223/how-to-convert-typescript-types-of-strings-to-array-of-strings
+export const ColorThemes = [
+  'app-red',
+  'app-critical',
+  'app-success',
+  'app-warning',
+  'app-decorative-red',
+  'app-decorative-blue',
+  'app-decorative-purple',
+  'app-background-black',
+  'app-background-blue',
+  'primary-brand',
+  'primary-primary',
+  'primary-secondary',
+  'primary-muted',
+  'primary-success',
+  'primary-danger',
+  'primary-warning',
+  'primary-info',
+  'primary-light',
+  'primary-hover',
+  'primary-dark',
+  'primary-transparent',
+  'secondary-brand',
+  'secondary-primary',
+  'secondary-secondary',
+  'secondary-muted',
+  'secondary-success',
+  'secondary-danger',
+  'secondary-warning',
+  'secondary-info',
+] as const;
+
+export type ColorThemeType = typeof ColorThemes[number];
