34118e851df6aa8d809c13592daba9dc5d83b518
Yuri Tsai <showwell0120@gmail.com>
2022-12-29 15:27:58 +0800

    feat(react-tabs): implement main features, add default story and doc

---
+++ apps/document/src/stories/react/display/tabs/tabs.stories.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ReactTabs, TabItem } from '@paintbox/react-tabs';
+
+export default {
+  component: ReactTabs,
+  title: 'React/Display/Tabs/Stories',
+} as ComponentMeta<typeof ReactTabs>;
+
+const Template: ComponentStory<typeof Tabs> = (args) => {
+  const [tabs, setTabs] = React.useState<TabItem[]>([
+    { id: 1, name: 'All' },
+    { id: 2, name: 'Video' },
+    { id: 3, name: 'Dubbing' },
+    { id: 4, name: 'Graphic' },
+    { id: 5, name: 'Subtitles' },
+  ]);
+  const [currentTabID, setCurrentTabID] = React.useState<number>(1);
+
+  return (
+    <ReactTabs
+      tabs={tabs}
+      currentTabID={currentTabID}
+      onTabChange={setCurrentTabID}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {};
--- packages/react/tabs/src/lib/react-tabs.tsx
+++ packages/react/tabs/src/lib/react-tabs.tsx
@@ -1,18 +1,50 @@
 import styled from '@emotion/styled';
-
-/* eslint-disable-next-line */
-export interface ReactTabsProps {}
-
-const StyledReactTabs = styled.div`
-  color: pink;
-`;
-
-export function ReactTabs(props: ReactTabsProps) {
-  return (
-    <StyledReactTabs>
-      <h1>Welcome to ReactTabs!</h1>
-    </StyledReactTabs>
-  );
+import { css } from '@emotion/react';
+export interface TabItem {
+  id: number;
+  name: string;
+  count?: number;
 }
 
-export default ReactTabs;
+/* eslint-disable-next-line */
+export interface ReactTabsProps {
+  tabs: TabItem[];
+  currentTabID: number;
+  onTabChange: (tabID: number) => void;
+}
+
+const Container = styled.div`
+  display: flex;
+  border-bottom: 1px solid var(--primary-muted);
+`;
+
+const tabStyle = css`
+  padding: 0.5rem 1rem;
+  color: var(--primary-muted);
+  cursor: pointer;
+`;
+
+const tabSelectedStyle = css`
+  color: var(--primary-primary);
+  border-bottom: 4px solid var(--primary-primary);
+`;
+
+export function ReactTabs({ tabs, currentTabID, onTabChange }: ReactTabsProps) {
+  return (
+    <Container>
+      {tabs.map((tab) =>
+        tab.count === undefined || tab.count !== 0 ? (
+          <div
+            key={tab.id}
+            css={[tabStyle, currentTabID === tab.id && tabSelectedStyle]}
+            onClick={() => onTabChange(tab.id)}
+          >
+            {tab.name}
+            &nbsp;
+            {tab.count !== undefined ? `(${tab.count})` : null}
+          </div>
+        ) : null
+      )}
+    </Container>
+  );
+}
