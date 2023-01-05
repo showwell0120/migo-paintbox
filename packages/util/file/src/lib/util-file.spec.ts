1b6e7fc71c5d719143702771448c8d18d05e4262
Yuri Tsai <showwell0120@gmail.com>
2023-01-05 16:08:07 +0800

    build: remove troubled package

--- packages/util/file/src/lib/util-file.spec.ts
+++
@@ -1,7 +0,0 @@
-import { utilFile } from './util-file';
-
-describe('utilFile', () => {
-  it('should work', () => {
-    expect(utilFile()).toEqual('util-file');
-  });
-});
--- packages/util/file/src/lib/util-file.ts
+++
@@ -1,3 +0,0 @@
-export const KB = 1024;
-export const MB = KB * 1024;
-export const GB = MB * 1024;
