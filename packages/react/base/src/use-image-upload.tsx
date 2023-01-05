c78a00ea6b34653fca67d96016bb4c28b9a8448d
Yuri Tsai <showwell0120@gmail.com>
2023-01-05 15:59:03 +0800

    .

--- packages/react/base/src/use-image-upload.tsx
+++ packages/react/base/src/use-image-upload.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-import { getDisplayedSize } from '@paintbox/util-file';
 import { Small } from '@paintbox/react-foundation';
 
 import { ImageFileProps } from '.';
+
+// TODO: move out
+const KB = 1024;
+const MB = KB * 1024;
+const GB = MB * 1024;
+
+export const getDisplayedSize = (bytes: number, digits = 2) => {
+  if (!bytes) {
+    return '0 B';
+  }
+  if (bytes < KB) {
+    return `${bytes} B`;
+  } else if (bytes >= KB && bytes < MB) {
+    return `${(bytes / KB).toFixed(digits)} KB`;
+  } else if (bytes >= MB && bytes < GB) {
+    return `${(bytes / MB).toFixed(digits)} MB`;
+  } else {
+    return `${(bytes / GB).toFixed(digits)} GB`;
+  }
+};
+
 
 const RemindText = styled.li`
   color: var(--primary-secondary);
--- packages/util/file/src/lib/util-file.ts
+++ packages/util/file/src/lib/util-file.ts
@@ -1,18 +1,3 @@
-const KB = 1024;
-const MB = KB * 1024;
-const GB = MB * 1024;
-
-export const getDisplayedSize = (bytes: number, digits = 2) => {
-  if (!bytes) {
-    return '0 B';
-  }
-  if (bytes < KB) {
-    return `${bytes} B`;
-  } else if (bytes >= KB && bytes < MB) {
-    return `${(bytes / KB).toFixed(digits)} KB`;
-  } else if (bytes >= MB && bytes < GB) {
-    return `${(bytes / MB).toFixed(digits)} MB`;
-  } else {
-    return `${(bytes / GB).toFixed(digits)} GB`;
-  }
-};
+export const KB = 1024;
+export const MB = KB * 1024;
+export const GB = MB * 1024;
